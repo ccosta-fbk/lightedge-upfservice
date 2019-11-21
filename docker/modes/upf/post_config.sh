a4aca88efdd0d1a4dc8b7f35bd1dcd63dbccdb45
g.baggio@create-net.org <g.baggio@create-net.org>
2019-11-21 14:16:34 +0100

    update makefile and clean config files

--- docker/modes/upf/post_config.sh
+++ docker/modes/upf/post_config.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-
 ip route add $enbs_subnet dev tun0
 iptables -t nat -A POSTROUTING -s 10.10.0.0/16 -o eth0 -j MASQUERADE
 iptables -t nat -A POSTROUTING -s 10.20.0.0/16 -o eth0 -j MASQUERADE
