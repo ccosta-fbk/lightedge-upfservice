0046e5f5d381960a8ae6ffd692a48b62deb12d0f
Roberto Riggio <robertoriggio@Robertos-MBP-2.station>
2020-02-06 22:21:33 +0100

    Remove dockerfile

--- docker/modes/upf/post_config.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-ip route add $enbs_subnet dev tun0
-iptables -t nat -A POSTROUTING -s 10.10.0.0/16 -o eth0 -j MASQUERADE
-iptables -t nat -A POSTROUTING -s 10.20.0.0/16 -o eth0 -j MASQUERADE
-ip route add 10.10.0.0/16 dev tun1
-ip route add 10.20.0.0/16 dev tun1
-
